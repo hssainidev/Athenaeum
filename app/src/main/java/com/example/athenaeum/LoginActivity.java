dbc5a044cabeef33016d24d0bb0368fa2bf782f3
Natalie Iwaniuk <natalieiwaniuk@gmail.com>
2020-10-26 15:38:54 -0600

    Create basic UI elements and navigation between Login and Signup pages.

---
+++ app/src/main/java/com/example/athenaeum/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.athenaeum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final Button signupButton = findViewById(R.id.buttonSignup);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
