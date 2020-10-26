a2a3812d6dccc43ef86e2da964d372a2a64ea160
RamboTheGreat7 <ramana@ualberta.ca>
2020-10-26 14:07:30 -0600

    initial outline of class attributes

---
+++ app/src/main/java/com/example/athenaeum/AthenaeumProfile.java
@@ -0,0 +1,7 @@
+package com.example.athenaeum;
+
+public class AthenaeumProfile {
+    private String username;
+    private String password;
+    private String email;
+}
---
+++ app/src/main/java/com/example/athenaeum/BookDB.java
@@ -0,0 +1,10 @@
+package com.example.athenaeum;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+public class BookDB {
+    private FirebaseFirestore booksDB;
+    private ArrayList<Book> books;
+}
