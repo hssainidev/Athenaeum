769ec5ec5f0de35211e25f7307cefbabc45ab913
Natalie Iwaniuk <natalieiwaniuk@gmail.com>
2020-11-06 10:46:49 -0700

    Adds placeholders for the menu buttons, adds header to menu, adds javadocs to user and book classes, adds functionality for logging out, outlines code for connecting Firebase to books list on home screen.

--- app/src/main/java/com/example/athenaeum/MainActivity.java
+++ app/src/main/java/com/example/athenaeum/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.athenaeum;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -11,12 +12,21 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -26,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<Book> bookAdapter;
     ArrayList<Book> bookDataList;
 
+
+    final String TAG = "Sample";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,21 +47,14 @@ public class MainActivity extends AppCompatActivity {
         bookList = findViewById(R.id.book_list);
         bookDataList = new ArrayList<>();
 
-        Book book1 = new Book();
-        Book book2 = new Book();
+        bookAdapter = new CustomBookList(this, bookDataList);
 
-        book1.setISBN("2472374");
-        book1.setTitle("Lorem Ipsum");
-        book1.setAuthor("Asdf Qwerty");
-
-        book2.setISBN("34727348");
-        book2.setTitle("Dolor Sit Amet");
-        book2.setAuthor("Asdf Qwerty");
+        // For testing only.
+        Book book1 = new Book("2472374", "Asdf Qwerty", "Lorem Ipsum");
+        Book book2 = new Book("34727348", "Asdf Qwerty", "Dolor Sit Amet");
 
         bookDataList.add(book1);
         bookDataList.add(book2);
-
-        bookAdapter = new CustomBookList(this, bookDataList);
 
         bookList.setAdapter(bookAdapter);
 
@@ -57,8 +63,43 @@ public class MainActivity extends AppCompatActivity {
         DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
         AppBarConfiguration appBarConfiguration =
                 new AppBarConfiguration.Builder(navController.getGraph())
-                        .setDrawerLayout(drawerLayout)
+                        .setOpenableLayout(drawerLayout)
                         .build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        if (menuItem.getItemId() == R.id.menu_logout) {
+                            finish();
+                        }
+                        return true;
+                    }
+                });
+
+        // Access a Cloud Firestore instance from your Activity
+        /*FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        final CollectionReference collectionReference = db.collection("Books");
+
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable
+                    FirebaseFirestoreException error) {
+                bookDataList.clear();
+                for(QueryDocumentSnapshot doc: queryDocumentSnapshots)
+                {
+                    Log.d(TAG, String.valueOf(doc.getData().get("ISBN")));
+                    String ISBN = (String) doc.getData().get("ISBN");
+                    String author = (String) doc.getData().get("Author");
+                    String title = (String) doc.getData().get("Title");
+                    bookDataList.add(new Book(ISBN, author, title)); // Adding the cities and provinces from FireStore
+                }
+                bookAdapter.notifyDataSetChanged(); // Notifying the adapter to render any new data fetched from the cloud
+            }
+        });*/
     }
 }
