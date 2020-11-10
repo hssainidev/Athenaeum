ca35862e80f818f354229f4a4a003517fe80a0ef
RamboTheGreat7 <ramana@ualberta.ca>
2020-11-10 16:46:59 -0700

    Linked UI to login and signup authentication with basic error checking

--- app/src/main/java/com/example/athenaeum/LoginActivity.java
+++ app/src/main/java/com/example/athenaeum/LoginActivity.java
@@ -1,18 +1,32 @@
 package com.example.athenaeum;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText pass;
+    EditText email;
+    UserAuth auth;
+    public Boolean verify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = findViewById(R.id.loginEmail);
+        pass = findViewById(R.id.loginPassword);
+        auth = new UserAuth();
 
         final Button signupButton = findViewById(R.id.buttonSignup);
         signupButton.setOnClickListener(new View.OnClickListener() {
@@ -25,9 +39,31 @@ public class LoginActivity extends AppCompatActivity {
         final Button loginButton = findViewById(R.id.buttonLogin);
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                verify = true;
                 // Add checking for correct data in login/signup
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+
+                if (verify) {
+                    Task<AuthResult> authentication = auth.signIn(email.getText().toString(), pass.getText().toString());
+                    authentication
+                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    String uid = authResult.getUser().getUid();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    verify = false;
+                                }
+                            });
+
+                }
+                if (verify) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+
             }
         });
     }
--- app/src/main/java/com/example/athenaeum/SignupActivity.java
+++ app/src/main/java/com/example/athenaeum/SignupActivity.java
@@ -1,25 +1,83 @@
 package com.example.athenaeum;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 public class SignupActivity extends AppCompatActivity {
+    UserDB userDB;
+    EditText pass;
+    EditText email;
+    EditText user;
+    UserAuth auth;
+    EditText passConfirm;
+    public Boolean verify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        userDB = new UserDB();
+        auth = new UserAuth();
+        pass = findViewById(R.id.signupPassword);
+        email = findViewById(R.id.signupEmail);
+        user = findViewById(R.id.signupUsername);
+        passConfirm = findViewById(R.id.signupPasswordConfirm);
 
         final Button signupButton = findViewById(R.id.buttonSignup);
         signupButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                verify = true;
+                String password = "";
+                String emailString = "";
+                String username = "";
                 // Do checking for data entered in the text boxes.
-                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-                startActivity(intent);
+                if (!(pass.getText().toString().equals(passConfirm.getText().toString()))) {
+                    verify = false;
+                    pass.setError("Passwords must match");
+                    passConfirm.setError("Passwords must match");
+                } else {
+                    password = pass.getText().toString();
+                }
+                emailString = email.getText().toString();
+                username = user.getText().toString();
+                if (verify) {
+                    final AthenaeumProfile profile = new AthenaeumProfile(username, password, emailString);
+
+                    Task<AuthResult> authentication = auth.addUser(profile);
+                    authentication
+                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    User user = new User(profile);
+                                    userDB.addUser(user, authResult.getUser().getUid());
+                                    Log.d("tag", "successfully added account");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d("Fail", e.toString());
+                                    verify = false;
+                                }
+                            });
+                }
+                if (verify) {
+                    Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
