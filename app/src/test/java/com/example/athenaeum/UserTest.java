4eda79988477926aea53be9fee2d84a9e2c0ff53
RamboTheGreat7 <ramana@ualberta.ca>
2020-11-30 15:14:34 -0700

    User unit tests

--- app/src/test/java/com/example/athenaeum/UserTest.java
+++ app/src/test/java/com/example/athenaeum/UserTest.java
@@ -5,22 +5,56 @@ import com.google.firebase.auth.AuthResult;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class UserTest {
-    private UserDB mockDB() {
-        return new UserDB();
-    }
-    private UserAuth mockAuth() {
-        return new UserAuth();
+
+    @Test
+    void testCreateUser() {
+        AthenaeumProfile profile = new AthenaeumProfile("Test", "TestName", "tester", "7801234567", "testing@gmail.com");
+        User user = new User(profile);
+        assertEquals(user.getProfile(), profile);
     }
 
     @Test
-    void testAdd() {
-        UserAuth auth=mockAuth();
-        UserDB db=mockDB();
-        AthenaeumProfile profile=new AthenaeumProfile("user","password","dummy@gmail.com");
-        Task<AuthResult> authentication=auth.addUser(profile);
-        User user=new User(profile);
-        db.addUser(user,authentication.getResult().getUser().getUid());
+    void testOverloadedProfile() {
+        AthenaeumProfile profile = new AthenaeumProfile("Test", "tester", "testing@gmail.com");
+        User user= new User(profile);
+        assertEquals(user.getProfile(), profile);
+    }
 
+    @Test
+    void testSetProfile() {
+        AthenaeumProfile wrongProfile= new AthenaeumProfile("Test1", "testing", "wrong@gmail.com");
+        AthenaeumProfile profile = new AthenaeumProfile("Test", "tester", "testing@gmail.com");
+        User user =new User(wrongProfile);
+        user.setProfile(profile);
+        assertEquals(user.getProfile(), profile);
+
+    }
+
+    @Test
+    void testAddBook() {
+        AthenaeumProfile profile = new AthenaeumProfile("Test", "tester", "testing@gmail.com");
+        User user= new User(profile);
+        Book book = new Book("123456789", "Author", "Name");
+        user.addBook(book.getISBN());
+        assertEquals(user.getBooks().size(),1);
+    }
+
+    @Test
+    void testSetBooks() {
+        AthenaeumProfile profile = new AthenaeumProfile("Test", "tester", "testing@gmail.com");
+        User user= new User(profile);
+        Book book = new Book("123456789", "Author", "Name");
+        ArrayList<String> books=new ArrayList<>();
+        books.add(book.getISBN());
+        user.setBooks(books);
+        assertEquals(user.getBooks().size(),1);
+        books.remove(book.getISBN());
+        user.setBooks(books);
+        assertEquals(user.getBooks().size(),0);
     }
 }
